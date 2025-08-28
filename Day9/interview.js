f657e8151c0d707e5994b405454f0cd22e0dc345
Sheraz Ahmad <sherrydeveloper01@gmail.com>
2025-08-28 12:09:07 +0500

    interview prep

---
+++ Day9/interview.js
@@ -0,0 +1,18 @@
+// code example practice
+// variable.
+if(true){
+    var x = 10;
+    let y = 10;
+    console.log(y);
+    const z = 10;
+}
+console.log(x);
+x = 20;
+console.log(x);
+
+function test(){
+    var a = 100;
+    console.log(a);
+}
+test();
+console.log(a);
