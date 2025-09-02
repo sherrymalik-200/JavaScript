93b120ca2cc15c6bfc3691006bff42efef1b3c17
Sheraz Ahmad <sherrydeveloper01@gmail.com>
2025-09-02 14:37:44 +0500

    close basics

--- Day9/interview.js
+++ Day9/interview.js
@@ -15,4 +15,52 @@ function test(){
     console.log(a);
 }
 test();
-console.log(a);
+
+console.log(null +1);
+console.log(true + false);
+console.log(typeof[]);
+console.log(typeof null);
+console.log(typeof 123n);
+
+
+// Reverse String
+
+function reverseString(str){
+    let reversed = "";
+    for(let i =str.length - 1; i >= 0; i--){
+        reversed += str[i];
+
+    }
+    return reversed;
+}
+console.log(reverseString("Sheraz"));
+
+//Lexical Scope
+let out = "sheraz";
+function outer(){
+    function inner(){
+        console.log(out);
+    }
+    inner();
+}
+outer();
+//closures.
+function Outer(){
+    let counter = 0;
+    function Inner(){
+        counter++;
+        console.log(counter);
+    }
+    return Inner;
+};
+let fn = Outer();
+fn();
+fn();
+
+
+// Array.
+let prices = [100, 200,300];
+let taxed = prices.map(p=>p*1.18)
+for(value in taxed){
+    console.log(taxed[value])
+}
